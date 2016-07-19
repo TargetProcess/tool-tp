fd85a6178565e2ec18a52a55c478f17b7bdff8c1
mavrin <krivlenia@gmail.com>
2016-07-19 21:43:12 +0300

    fix code style

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	id: 'tp',
-	port: 3010,
-	mongo: {host: process.env.MONGO_PORT_27017_TCP_ADDR, port: process.env.MONGO_PORT_27017_TCP_PORT, db: 'tp'},
-	secretKey: 'secretKey',
-	url: null,
-	buildboardUrl: null
+    id: 'tp',
+    port: 3010,
+    mongo: {host: process.env.MONGO_PORT_27017_TCP_ADDR, port: process.env.MONGO_PORT_27017_TCP_PORT, db: 'tp'},
+    secretKey: 'secretKey',
+    url: null,
+    buildboardUrl: null
 };
